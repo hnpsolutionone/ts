ad87f65aba69db36972f39555e6fbc96441180b4
Nguyen Le Phuong <hnpsolution@gmail.com>
2017-05-20 18:33:06 +0700

    apply try cache

--- solve.php
+++ solve.php
@@ -1,20 +1,24 @@
 <?php
 require_once 'TS.class.php';
 
-$ts = new TS;
+try {
+    $ts = new TS;
 
 // open file cities.txt and stored information of file into array
-$ts->storeMatrix('cities.txt');
+    $ts->storeMatrix('cities.txt');
 
 // init the cost matrix moves between cities.
-$ts->initMatrix();
+    $ts->initMatrix();
 
 // calculator to find shortest route base on Nearest neighbor algorithm and begin with Beijing (=1) city .
-$ts->compute(count($ts->locations), 1 , $ts->cost_matrix);
+    $ts->compute(count($ts->locations), 1 , $ts->cost_matrix);
 
 // print the result shortest distance and shortest route
-echo "\nShortest Distance: " . $ts->cost . "\n\n";
-echo "Shortest Route:\n";
-$ts->printRoute();
-echo "\n\n";
+    echo "\nShortest Distance: " . $ts->cost . "\n\n";
+    echo "Shortest Route:\n";
+    $ts->printRoute();
+    echo "\n\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
 ?>
