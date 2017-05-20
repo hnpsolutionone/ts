10f0136b722649a2dee584da9ef281b98911d327
hnpsolutionone <hnpsolution@gmail.com>
2017-05-20 13:53:00 +0700

    Add files via upload

--- unit_test.php
+++ unit_test.php
@@ -15,6 +15,8 @@ class TSTest extends PHPUnit_Framework_TestCase {
         $ts_test->add(1,'Beijing AAA',39.93,116.40);
         $ts_test->add(2,'Tokyo',35.40,139.45);
         $ts_test->add(3,'Vladivostok',43.8,131.54);
+        $ts_test->add(4,'Dakar',14.40,-17.28);
+        $ts_test->add(5,'Singapore',1.14,103.55);
         $ts_test->initMatrix();
         // calculator begin with Beijing city.
         $ts_test->compute(count($ts_test->locations), 1 , $ts_test->cost_matrix);
