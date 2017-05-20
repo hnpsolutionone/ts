813e43488078c8d7514372560f295c348769fdf7
hnpsolutionone <hnpsolution@gmail.com>
2017-05-20 13:17:49 +0700

    Add files via upload

--- solve.php
+++ solve.php
@@ -25,8 +25,9 @@ class TS {
      * @param int $longitude The longitude of city
      * @param int $latitude The latitude of city
      */
-    public function add($i,$name,$longitude,$latitude){
-        $this->locations[$i] = array('name'=>$name,'longitude'=>$longitude,'latitude'=>$latitude);
+    public function add($i,$name,$longitude,$latitude)
+    {
+        $this->locations[$i] = array('name'=>$name, 'longitude'=>$longitude, 'latitude'=>$latitude);
     }
 
     /**
@@ -34,16 +35,16 @@ class TS {
      *
      * @param File $file_name A cities text
      */
-    public function store_matrix($file_name){
+    public function store_matrix($file_name)
+    {
         if ($file = fopen($file_name, "r")) {
             $i = 1;
             while(!feof($file)) {
                 $line = fgets($file);
                 $address = explode(" ", trim($line));
-                if(count($address) == 3) {
+                if (count($address) == 3) {
                     $this->add($i, $address[0], $address[1], $address[2]);
-                }
-                elseif(count($address) == 4){
+                } elseif (count($address) == 4){
                     $this->add($i, $address[0] . ' ' . $address[1], $address[2], $address[3]);
                 }
                 $i++;
@@ -55,14 +56,15 @@ class TS {
     /**
      * Init the cost matrix moves between cities
      */
-    public function init_matrix(){
+    public function init_matrix()
+    {
         $num_locations = count($this->locations);
         for ($i = 1; $i <= $num_locations; $i++) {
             for ($j = 1; $j <= $num_locations; $j++) {
-                if($i == $j) {
+                if ($i == $j) {
                     $this->cost_matrix[$i][$j] = $this->infinity;
-                }else{
-                    $this->cost_matrix[$i][$j] = $this->distance($this->locations[$i]['latitude'],$this->locations[$i]['longitude'],$this->locations[$j]['latitude'],$this->locations[$j]['longitude']);
+                } else {
+                    $this->cost_matrix[$i][$j] = $this->distance($this->locations[$i]['latitude'], $this->locations[$i]['longitude'], $this->locations[$j]['latitude'], $this->locations[$j]['longitude']);
                 }
             }
         }
@@ -71,8 +73,9 @@ class TS {
     /**
      * Show the all cities which have visited
      */
-    public function print_route(){
-        foreach(end($this->visited) as $visit) {
+    public function print_route()
+    {
+        foreach (end($this->visited) as $visit) {
             print $this->locations[$visit]['name'] . '<br />' ;
         }
     }
@@ -84,23 +87,24 @@ class TS {
      * @param int $v The start point of tour
      * @param int $mt_cities The matrix include all cost between cities
      */
-    public function compute($n, $v, $mt_cities){
-        for($i = 0; $i <= $n; $i++)
-            $flag[$i]=0;
+    public function compute($n, $v, $mt_cities)
+    {
+        for ($i = 0; $i <= $n; $i++)
+            $flag[$i] = 0;
 
-        $t=$v;
-        $count=0;
-        $tour[$t][0]=$v;
-        $cost=0;
-        $flag[$v]=1;
-        $tmp=$v;
+        $t = $v;
+        $count = 0;
+        $tour[$t][0] = $v;
+        $cost = 0;
+        $flag[$v] = 1;
+        $tmp = $v;
 
-        while($count != $n-1)
+        while ($count != $n-1)
         {
             $tmp_cost = $this->infinity;
-            for($i = 1; $i <= $n; $i++)
+            for ($i = 1; $i <= $n; $i++)
             {
-               if($tmp_cost > $mt_cities[$v][$i] && $flag[$i]==0 && $mt_cities[$v][$i] != -1)
+               if ($tmp_cost > $mt_cities[$v][$i] && $flag[$i] == 0 && $mt_cities[$v][$i] != -1)
                {
                    $tmp_cost = $mt_cities[$v][$i];
                    $yes = $i;
@@ -125,7 +129,8 @@ class TS {
      * @param int $lat2 The latitude of City 2
      * @param int $lon2 The longitude of City 2
      */
-	function distance($lat1, $lon1, $lat2, $lon2) {
+	function distance($lat1, $lon1, $lat2, $lon2)
+    {
 		if ($lat1 == $lat2 && $lon1 == $lon2) return 0;
 		$unit = 'M';	// miles please!
 		$theta = $lon1 - $lon2; 
